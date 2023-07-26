440101024b50e7ed885ce4b8f6b75f9156fa1673
Dhurav kumar <dhuravmaheshpur@gmail.com>
2023-07-26 20:45:36 +0530

    error image uploading

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,35 @@
-const User=require("../models/userModel");
+const User = require("../models/userModel");
 
-const loadRegister=async(req,res)=>{
-    try{
-        res.render('registration');
-    }catch(error){
-        console.log(error.message);
-    }
-}
+const loadRegister = async (req, res) => {
+  try {
+    res.render("registration");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports={
-    loadRegister
-}
+const insertUser = async (req, res) => {
+  try {
+   const user= new User({
+        name:req.body.name,
+        email:req.body.email,
+        mobile:req.body.mobile,
+        image:req.file.filename,
+        password:req.body.password,
+        is_admin:0
+    });
+   const data=await user.save();
+   if(data){
+    res.render('registration',{message:"Your registration has been successfully"});
+   }else{
+    res.render('registration',{message:"Your registration has been failed"});
+   }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = {
+  loadRegister,
+  insertUser
+};
